5629c9956f191342f8188c53ef8faa2aeeefb8f2
RS-TEST <rs-test@test.localdomain>
2024-12-23 11:50:42 -0600

    Updated Utilitytest class to get system property

--- src/test/java/org/example/library/UtilityTest.java
+++ src/test/java/org/example/library/UtilityTest.java
@@ -1,14 +1,13 @@
 package org.example.library;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class UtilityTest {
 
     @Test
     void testGreetWithJenkinsParameter() {
-        String name = System.getenv("TEST_NAME"); // Get the parameter from Jenkins
+        String name = System.getProperty("TEST_NAME"); // Get the parameter from Maven
         if (name == null || name.isBlank()) {
             assertThrows(IllegalArgumentException.class, () -> Utility.greet(name));
         } else {
